3909a4d1fee8ceae09cf59c8b91828df33131485
metrapak <pokachajloartem@gmail.com>
2026-02-05 10:23:15 +0100

    Test routes

--- src/src/Controller/TestController.php
+++ src/src/Controller/TestController.php
@@ -6,6 +6,7 @@ use App\Service\MathService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -29,5 +30,44 @@ class TestController extends AbstractController
         $sum = $math->add(2, 3);
 
         return new Response("2 + 3 = $sum");
+    }
+
+    /**
+     * This route has a greedy pattern and is defined first.
+     */
+    #[Route(
+        '/blog/{slug}',
+        name: 'blog_show',
+        requirements: ['slug' => '.+'],
+        defaults: ['slug' => 'test'],
+        methods: ['GET'],
+    )]
+    public function show(Request $request, string $slug): Response
+    {
+        $routeName = $request->attributes->get('_route');
+        $routeParameters = $request->attributes->get('_route_params');
+        $allAttributes = $request->attributes->all();
+
+        return $this->json([
+            'Showing blog post: '.$slug,
+        ]);
+    }
+
+    /**
+     * This route could not be matched without defining a higher priority than 0.
+     */
+    #[Route(
+        path: [
+            'en' => '/blog/list',
+            'nl' => '/blog/list-nl',
+            '/blog/list',
+        ],
+        name: 'blog_list',
+        priority: 2)]
+    public function list(): Response
+    {
+        $this->generateUrl('blog_show');
+
+        return $this->json(['Showing blog list']);
     }
 }
