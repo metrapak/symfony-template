6648115564a70d4d3d4a68c42174888227e93414
metrapak <pokachajloartem@gmail.com>
2026-02-23 15:32:55 +0100

    More features in controllers

--- src/src/Shared/Infrastructure/Controller/MainController.php
+++ src/src/Shared/Infrastructure/Controller/MainController.php
@@ -10,6 +10,7 @@ use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -72,5 +73,23 @@ class MainController extends AbstractController
         $request->isXmlHttpRequest(); // Is it an AJAX request?
 
         return $response;
+    }
+
+    #[Route('/generate-url/{param?}', name: 'generate_url')]
+    public function generateUrlCustom(): never
+    {
+        exit($this->generateUrl('generate_url', ['param' => 10]));
+    }
+
+    #[Route('/redirect-test', name: 'redirect_test')]
+    public function redirectTest(): RedirectResponse
+    {
+        return $this->redirectToRoute('generate_url');
+    }
+
+    #[Route('/forward-test', name: 'forward_test')]
+    public function forwardController(): Response
+    {
+        return $this->forward('App\Shared\Infrastructure\Controller\MainController::generateUrlCustom');
     }
 }
