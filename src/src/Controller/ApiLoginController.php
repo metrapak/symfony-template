4cb50cf4074fa676cc9530f6114cff7dbd2d74c0
metrapak <pokachajloartem@gmail.com>
2026-02-26 09:07:51 +0100

    Api login

---
+++ src/src/Controller/ApiLoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+final class ApiLoginController extends AbstractController
+{
+    #[Route('/api/login', name: 'api_login', methods: ['POST'])]
+    public function index(#[CurrentUser] ?User $user): Response
+    {
+
+        if (null === $user) {
+            return $this->json([
+                'message' => 'missing credentials',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $token = '...'; // somehow create an API token for $user
+
+        return $this->json([
+            'user' => $user->getUserIdentifier(),
+            'token' => $token,
+        ]);
+    }
+}
