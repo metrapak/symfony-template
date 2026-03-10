f4cdd28c3e56f38859f859c8cf08367462691b54
metrapak <pokachajloartem@gmail.com>
2026-03-10 10:57:26 +0100

    Test for edit/delete categories

--- src/tests/Videos/Controller/AdminControllerCategoriesTest.php
+++ src/tests/Videos/Controller/AdminControllerCategoriesTest.php
@@ -54,4 +54,53 @@ class AdminControllerCategoriesTest extends WebTestCase
         $this->assertNotNull($category);
         $this->assertSame($category_name, $category->getName());
     }
+
+    public function testEditCategory()
+    {
+        $new_category = new Category();
+        $new_category->setName('Category to Edit');
+        $this->entityManager->persist($new_category);
+        $this->entityManager->flush();
+
+        $id = $new_category->getId();
+
+        $crawler = $this->client->request('GET', '/videos/admin/edit-category/' . $id);
+        $this->assertResponseIsSuccessful();
+
+        $category_name = 'Updated Category Name';
+        $form = $crawler->selectButton('Save')->form([
+            'category[name]' => $category_name,
+            'category[parent]' => 1,
+        ]);
+
+        $this->client->submit($form);
+        $this->assertResponseRedirects();
+        $this->client->followRedirect();
+
+        $this->entityManager->clear();
+
+        $category = $this->entityManager->getRepository(Category::class)->find($id);
+
+        $this->assertNotNull($category);
+        $this->assertSame($category_name, $category->getName());
+    }
+
+    public function testDeleteCategory(): void
+    {
+        $category = new Category();
+        $category->setName('Category to Delete');
+        $this->entityManager->persist($category);
+        $this->entityManager->flush();
+
+        $id = $category->getId();
+
+        $this->client->request('GET', '/videos/admin/delete-category/' . $id);
+        $this->assertResponseRedirects();
+        $this->client->followRedirect();
+
+        $this->entityManager->clear();
+
+        $deleted = $this->entityManager->getRepository(Category::class)->find($id);
+        $this->assertNull($deleted);
+    }
 }
